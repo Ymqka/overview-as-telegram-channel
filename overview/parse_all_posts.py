de3af128b042bcf0d2d48e2e78d18da75773c3d3
Your Name <you@example.com>
2020-07-19 16:13:51 +0300

    added some logging

--- overview/parse_all_posts.py
+++ overview/parse_all_posts.py
@@ -4,12 +4,20 @@ from bs4 import BeautifulSoup
 
 import time
 import sys
+import logging
 
 import parser
 import utils
 
 args   = utils.parse_arguments()
 config = utils.parse_config(args.config)
+
+logging.basicConfig(
+    filename=config['utils']['log_filepath'],
+    level=logging.INFO, 
+    format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+    datefmt='%H:%M:%S'
+)
 
 overview_db = utils.OverView_db(config['overview'])
 
@@ -34,25 +42,30 @@ while True:
         url_without_params, url_with_proper_params = parser.parse_link_from_html(link_html, parser_config['img_params'])
 
         path_to_image, epoch_id = parser.get_link_info(url_without_params, parser_config['img_storage'])
-        
+
         parser.download_image(url_with_proper_params, path_to_image)
 
         links.append({
             "path_to_image": path_to_image,
             "epoch_id":      int(epoch_id)
         })
+        logging.info('going to sleep 3 secs')
         time.sleep(3)
+
 
     if(len(links) == len(posts)):
         for i in range(0, len(links)):
             post = {**(posts[i]), **(links[i])}
             total_posts.append(post)
+            logging.info("succesfully processed " + len(posts) + ' posts')
     else:
         logging.error('length of links and length of posts are not equal, url: ' + driver.current_url)
 
-    break
     driver.find_element_by_xpath("//*[@id='w-node-e4d10996b7f3-c6b3f613']/a").click()
     soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    logging.info('going to sleep 10 secs')
     time.sleep(10)
 
+logging.info('succesfully processed ' + len(total_posts) + ' posts')
 overview_db.add_posts(total_posts)
